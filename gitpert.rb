429e0423e74092fe9e9a0c7f74424f310b25dbca
augmentable <patrick@augmentable.io>
2020-04-22 18:19:27 -0400

    Brew formula update for gitpert version v0.0.6

--- gitpert.rb
+++ gitpert.rb
@@ -2,16 +2,16 @@
 class Gitpert < Formula
   desc "measure the pertinence of git authors as a time-decayed measure of LOC added and removed to a repository (or a set of files in a repository)"
   homepage "https://github.com/augmentable-dev/gitpert/"
-  version "0.0.5"
+  version "0.0.6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/augmentable-dev/gitpert/releases/download/v0.0.5/gitpert_0.0.5_Darwin_x86_64.tar.gz"
-    sha256 "3ba0db2a4d3cd0ac98430d0530af189e283ca1bf84964105a5f398350b56052e"
+    url "https://github.com/augmentable-dev/gitpert/releases/download/v0.0.6/gitpert_0.0.6_Darwin_x86_64.tar.gz"
+    sha256 "8defd5e142f62506fb90b77f4d16d1ead2fba5ba81c3316de93d9a569df9ecf1"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/augmentable-dev/gitpert/releases/download/v0.0.5/gitpert_0.0.5_Linux_x86_64.tar.gz"
-      sha256 "48741d3376df24df12b19092d32d59f43017cc1ba0f0e450fb63cf46b58b4ee1"
+      url "https://github.com/augmentable-dev/gitpert/releases/download/v0.0.6/gitpert_0.0.6_Linux_x86_64.tar.gz"
+      sha256 "184e320fd1da31aa7963fa624000a12d99d142069fe2b956e50a30ceac70c420"
     end
   end
 
